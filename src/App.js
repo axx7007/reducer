5c742d01faa40ff69043cb98c2c5f19b5a27642f
abduvaxobovxojiakbar7007 <xojiakbarabduvaxobov7007@gmail.com>
2021-05-09 16:05:48 +0500

    dispatch, reducer, actiontype learned

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useReducer} from 'react'
+import {Container} from './AppStyle'
 function App() {
+ 
+  const reducer=(state, action)=>{
+    switch(action.type){
+      case 'decrement': return state-1;
+      case 'increment': return state+1;
+      default: return state
+    }
+  }
+
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
-    <div>
-      App.js
-    </div>
+    <Container>
+      <button onClick={()=>dispatch({type:'decrement'})}>-</button>
+      <h5>{count}</h5>      
+      <button onClick={()=>dispatch({type: 'increment'})}>+</button>      
+    </Container>
   );
 }
 
