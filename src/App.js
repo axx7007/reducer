7ece6ad86f6f2905adafa137c75510d84430c43c
abduvaxobovxojiakbar7007 <xojiakbarabduvaxobov7007@gmail.com>
2021-05-11 00:47:31 +0500

    reducer

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ function App() {
  
   const reducer=(state, action)=>{
     switch(action.type){
-      case 'decrement': return state-1;
-      case 'increment': return state+1;
-      default: return state
+      case "decrement": return state-1;
+      case "increment": return state+1;
+      default: return  state
     }
   }
-
   const [count, dispatch] = useReducer(reducer, 0);
-
   return (
     <Container>
-      <button onClick={()=>dispatch({type:'decrement'})}>-</button>
-      <h5>{count}</h5>      
-      <button onClick={()=>dispatch({type: 'increment'})}>+</button>      
+      <button onClick={()=>dispatch({type: "decrement"})}>-</button>
+      <h2>{count}</h2>  
+      <button onClick={()=>dispatch({type: "increment"})}>+</button>
     </Container>
   );
 }
